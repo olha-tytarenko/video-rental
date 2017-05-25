90fa3162e4a42c52abe6bd2bcc467688842e5306
olya19 <titarenko.olga.1997@gmail.com>
2017-05-25 14:48:12 +0300

    removed bootstrap

--- VideoRentalSite/App_Start/BundleConfig.cs
+++ VideoRentalSite/App_Start/BundleConfig.cs
@@ -20,11 +20,12 @@ namespace VideoRentalSite
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/video-details.css",
+                      "~/Content/main-video-styles.css",
                       "~/Content/site.css"));
         }
     }
